012552019b7ffa9fc0c95d4aada9d3cabbfa4401
AshwaniKRawat <rawat.ashwani@gmail.com>
2023-03-03 00:05:45 +0530

    adding file after main task completed

--- git_stash.java
+++ git_stash.java
@@ -1,4 +1,10 @@
 This file I have created for git Stash demo
+<<<<<<< Updated upstream
  
 new Version assigned by my manager and I hold my old task
  now  this is new version of file.
+=======
+
+working on my task 1.10
+this is first assigned task now I got new task from manager so I am going to hold this task by git stash command
+>>>>>>> Stashed changes
